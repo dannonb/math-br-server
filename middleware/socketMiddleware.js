e36c1755ef3ff55c775ee4d7296c7f80e472ba2a
dannon <53214621+dannonb@users.noreply.github.com>
2023-07-10 11:08:49 -0700

    logging socket id

--- middleware/socketMiddleware.js
+++ middleware/socketMiddleware.js
@@ -4,6 +4,7 @@ import Player from '../models/player.js'
 export const socketAuth = async (socket, next) => {
     try {
         const token = socket.handshake.headers.authtoken
+        console.log(token)
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const player = await Player.findOne({ _id: decoded._id, 'tokens.token': token })
 
