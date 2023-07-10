592f6325d2e068f9acb1493b6b6d9b4fd2537cdf
dannon <53214621+dannonb@users.noreply.github.com>
2023-07-10 02:13:08 -0700

    fix bug with login

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,7 @@ import Player from '../models/player.js'
 
 const auth = async (req, res, next) => {
     try {
+        console.log(req.header)
         const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const player = await Player.findOne({ _id: decoded._id, 'tokens.token': token })
